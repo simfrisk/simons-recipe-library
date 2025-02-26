b6b33a2c6c2a1a1cc9216d78751c983c98ed9410
simfrisk <simoncarlfrisk@gmail.com>
2025-02-26 08:43:32 +0100

    Organized and made cleaner code

--- script/script.js
+++ script/script.js
@@ -1,13 +1,22 @@
+// ----- Sort Kitchen --- Manual Fold -----
+
+// ----- Sort Kitchen Varibles --- Manual Fold ----- 
 const allBtn = document.getElementById("all-btn")
 const italyBtn = document.querySelector("#italy-btn")
 const usaBtn = document.querySelector("#usa-btn")
-const chinaBtn = document.querySelector("#china-btn")
+const asiaBtn = document.querySelector("#asia-btn")
 
+const italyCard = document.querySelector("#italy-card")
+const usaCard = document.querySelector("#usa-card")
+const asiaCard = document.querySelector("#asia-card")
 
+// ----- Sort Kitchen btn function --- Manual Fold -----
 allBtn.onclick = () => {
+  // Add text to document
+
   italyBtn.classList.remove("kitchen-btn-active")
   usaBtn.classList.remove("kitchen-btn-active")
-  chinaBtn.classList.remove("kitchen-btn-active")
+  asiaBtn.classList.remove("kitchen-btn-active")
   if (allBtn.classList.contains("kitchen-btn-active")) {
     allBtn.classList.remove("kitchen-btn-active")
   } else {
@@ -18,39 +27,41 @@ allBtn.onclick = () => {
 italyBtn.onclick = () => {
   allBtn.classList.remove("kitchen-btn-active")
   usaBtn.classList.remove("kitchen-btn-active")
-  chinaBtn.classList.remove("kitchen-btn-active")
-  if (italyBtn.classList.contains("kitchen-btn-active")) {
-    italyBtn.classList.remove("kitchen-btn-active")
-  } else {
-    italyBtn.classList.add("kitchen-btn-active")
-  }
+  asiaBtn.classList.remove("kitchen-btn-active")
+
+  usaCard.classList.toggle("hidden")
+  asiaCard.classList.toggle("hidden")
+
+  italyBtn.classList.toggle("kitchen-btn-active")
 }
 
 usaBtn.onclick = () => {
   allBtn.classList.remove("kitchen-btn-active")
   italyBtn.classList.remove("kitchen-btn-active")
-  chinaBtn.classList.remove("kitchen-btn-active")
-  if (usaBtn.classList.contains("kitchen-btn-active")) {
-    usaBtn.classList.remove("kitchen-btn-active")
-  } else {
-    usaBtn.classList.add("kitchen-btn-active")
-  }
+  asiaBtn.classList.remove("kitchen-btn-active")
+  italyCard.classList.toggle("hidden")
+  asiaCard.classList.toggle("hidden")
+
+  usaBtn.classList.toggle("kitchen-btn-active")
 }
 
-chinaBtn.onclick = () => {
+asiaBtn.onclick = () => {
   allBtn.classList.remove("kitchen-btn-active")
   italyBtn.classList.remove("kitchen-btn-active")
   usaBtn.classList.remove("kitchen-btn-active")
-  if (chinaBtn.classList.contains("kitchen-btn-active")) {
-    chinaBtn.classList.remove("kitchen-btn-active")
-  } else {
-    chinaBtn.classList.add("kitchen-btn-active")
-  }
+  italyCard.classList.toggle("hidden")
+  usaCard.classList.toggle("hidden")
+
+  asiaBtn.classList.toggle("kitchen-btn-active")
 }
 
+// ----- Sort Time --- Manual Fold -----
+
+// ----- Sort Time Varibles --- Manual Fold -----
 const decendBtn = document.querySelector("#decend-btn")
 const acendBtn = document.querySelector("#acend-btn")
 
+// ----- Sort Time Varibles --- Manual Fold -----
 decendBtn.onclick = () => {
   acendBtn.classList.remove("time-btn-active")
   if (decendBtn.classList.contains("time-btn-active")) {
@@ -68,4 +79,7 @@ acendBtn.onclick = () => {
     acendBtn.classList.add("time-btn-active")
   }
 }
+
+
+
 
